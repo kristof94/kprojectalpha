f86cf76d7b359a5c91a0d861e57f7f333b23d590
kristof94 <christophe-dossantos@outlook.com>
2018-01-20 16:17:48 +0100

    init project

---
+++ initGit.sh
@@ -0,0 +1,7 @@
+echo "# kprojectalpha" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/kristof94/kprojectalpha.git
+git push -u origin master
+
